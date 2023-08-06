1c72c92f3a5b5c1efe89a3da015d6a9bb81b72b0
scorpion <126881227+jujuCEEPfrontend@users.noreply.github.com>
2023-08-06 16:39:35 -0300

    Add files via upload

---
+++ 2-trabalhandoComVariáveis.js
@@ -0,0 +1,12 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 29;
+const nome = "Julia";
+let ano = 2020;
+
+//não fazer
+mes= "Janeiro";
+
+console.log(idade)
---
+++ 5-AtribuiçãoDeVariáveisw.js
@@ -0,0 +1,19 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 29;
+const primeironome = "Julia";
+const sobrenome ="Scarpim";
+
+//console.log(nome + "" + sobrenome)
+console.log(primeironome, sobrenome)
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador +1;
+
+const nomeCompleto = primeironome + sobrenome;
+console.log(nomeCompleto);
+
+letidade; //declarando variável
+idade = 26; //atribuindo valor
+idade = idade+1;
+console.log(idade);
